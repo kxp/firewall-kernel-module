b099062c9b1d37c22cd58102efe471bd3e066985
kxp <andre.cachopas@gmail.com>
2019-07-11 23:49:22 +0100

    Cleanup
    Added: print IP of accepted connections

--- minifw.c
+++ minifw.c
@@ -14,15 +14,14 @@ unsigned int telnetFilter(void *priv, struct sk_buff *skb,
 	struct iphdr *iph;
 	struct tcphdr *tcph;
 	int port;
-	unsigned int src_ip ;
-	unsigned int dest_ip ;
-	
+	//unsigned int src_ip ;
+	//unsigned int dest_ip ;
 	
 	iph = ip_hdr(skb);
 	tcph = (void *)iph+iph->ihl*4;
 
 	port = tcph->dest;
-	printk(KERN_INFO "packet port%d\n", port);
+	//printk(KERN_INFO "Destination port: %d\n", port);
 
 	//objectives:
 	//block telnet protocol between two machines
@@ -38,21 +37,20 @@ unsigned int telnetFilter(void *priv, struct sk_buff *skb,
 	} 
 	
 	//struct iphdr *ip_header = (struct iphdr *)skb_network_header(skb);
-	src_ip = (unsigned int)iph->saddr;
-	dest_ip = (unsigned int)iph->daddr;
-	//printk(KERN_DEBUG "Received packet from source address: %pI4\n",src_ip);
-	//printk(KERN_DEBUG "A packet has a destination address of: %d.%d.%d.%d!\n",NIPQUAD(dest_ip));
-	
-	/* else {
-		return NF_ACCEPT;
-	}*/
+	//src_ip = (unsigned int)iph->saddr;
+	//dest_ip = (unsigned int)iph->daddr;
+	//printk(KERN_INFO "Received packet from source address: %pI4\n",src_ip);	
+	printk(KERN_INFO "Accepted packet to %d.%d.%d.%d\n",
+		((unsigned char *)&iph->daddr)[0],
+		((unsigned char *)&iph->daddr)[1],
+		((unsigned char *)&iph->daddr)[2],
+		((unsigned char *)&iph->daddr)[3]);
+
 	return NF_ACCEPT;
 }
 
 
 int setUpFilter(void) {
-	
-	
 	printk(KERN_INFO "starting a Telnet filter init.\n");
 
 	telnetFilterHook.hook = telnetFilter; 
@@ -65,8 +63,6 @@ int setUpFilter(void) {
 	printk(KERN_INFO "Telnet init st ptr: %p.\n", &telnetFilterHook );
 	printk(KERN_INFO "Telnet init hook addr:%p.\n", telnetFilterHook.hook);
 
-
-
 	// Register the hook.
 	nf_register_net_hook(&init_net, &telnetFilterHook );
 	return 0;
@@ -77,7 +73,6 @@ void removeFilter(void) {
 	nf_unregister_net_hook( &init_net, &telnetFilterHook );
 	printk(KERN_INFO "Telnet filter pos removal.\n");
 }
-
 
 
 module_init(setUpFilter);
